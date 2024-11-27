3ddaf3b033d43b95231ac613a8632a3bf042966e
Daewook123 <cdo5080@gmail.com>
2024-11-27 10:06:22 +0900

    RED

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,6 +3,7 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-
+        Calc calc = new Calc();
+        calc.run("");
     }
 }
--- src/test/java/PolynomialCalculatorTest.java
+++ src/test/java/PolynomialCalculatorTest.java
@@ -1,11 +1,34 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PolynomialCalculatorTest {
 
     @Test
-    @DisplayName("괄호")
+    @DisplayName("계산")
     public void t1(){
+        //given
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+        Calc calc = new Calc(TestUtil.genScanner("""
+                ((3 + 5) * 5 + -10) * 10 / 5
+                """));
 
+        //when
+        int rs = calc.run();
+        String output = outputStream.toString();
+
+        //then
+        assertThat(output)
+                .contains("1번째 계산 : 3 + 5")
+                .contains("2번째 계산 : 8 * 5")
+                .contains("3번째 계산 : 40 - 10")
+                .contains("4번째 계산 : 30 * 10")
+                .contains("5번째 계산 : 300 / 5")
+                .contains("결과 : 60");
+
+        TestUtil.clearSetOutToByteArray(outputStream);
     }
 }
