5b728712374cc9015835de6aa1b911fc6237a015
ByeongGyu Jeon <cube9928@naver.com>
2024-11-27 09:26:02 +0900

    3+5 RED

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,16 @@
+import com.ll.Calc;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalcTest {
+    @Test
+    @DisplayName("3 + 5")
+    public void t1() {
+        int rs = Calc.run("3 + 5");
+        assertThat(rs).isEqualTo(8);
+    }
+
+}
