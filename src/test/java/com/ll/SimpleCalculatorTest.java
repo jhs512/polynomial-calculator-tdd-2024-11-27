e67730891bb4877f09a3383a1b496d1698653c76
UCLABgown <lovecein4858@gmail.com>
2024-11-27 10:41:11 +0900

    assertThat 사용

--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -4,20 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 public class SimpleCalculatorTest {
     @Test
     @DisplayName("1 + 2 =3")
     public void testPlus(){
         SimpleCaculator simple = new SimpleCaculator();
         int rs = simple.plus(1,2);
-        assertEquals(3,rs);
+        assertThat(rs).isEqualTo(3);
     }
     @Test
     @DisplayName("6 + 8 =14")
     public void testPlus2(){
         SimpleCaculator simple = new SimpleCaculator();
         int rs = simple.plus(6,8);
-        assertEquals(14,rs);
+        assertThat(rs).isEqualTo(14).isEqualTo(6);
     }
 }
