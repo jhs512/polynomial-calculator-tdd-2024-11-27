0a80d61402101ebc3182d2b28074c0cbec79dceb
UCLABgown <lovecein4858@gmail.com>
2024-11-27 10:18:43 +0900

    실패하는 테스트 추가

---
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleCalculatorTest {
+    @Test
+    @DisplayName("1 + 2 =3")
+    public void testPlus(){
+        SimpleCaculator simple = new SimpleCaculator();
+        int rs = simple.plus(1,2);
+        assertEquals(3,rs);
+    }
+}
