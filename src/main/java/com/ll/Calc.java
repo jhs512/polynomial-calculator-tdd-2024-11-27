e99496aa51660c34612b6cc614c70f2167283346
ByeongGyu Jeon <cube9928@naver.com>
2024-11-27 09:28:48 +0900

    3+5 BLUE

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,7 +1,10 @@
 package com.ll;
 
 public class Calc {
-    public static int run(String s) {
-        return 8;
+    public static int run(String exp) {
+        // 3 + 5 를 " + " 로 자르기
+        String[] terms = exp.split(" \\+ ");
+
+        return Integer.parseInt(terms[0]) + Integer.parseInt(terms[1]);
     }
 }
