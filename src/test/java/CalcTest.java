72a663b2d51412cbbe0eed524f44072d0a3169f5
ByeongGyu Jeon <cube9928@naver.com>
2024-11-27 11:26:10 +0900

    0으로 나누는 테스트케이스 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -3,34 +3,43 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalcTest {
     // int rs = Calc.run("((3 + 5) * 5 + -10) * 10 / 5");
 
     @Test
     @DisplayName("3 + 5")
-    public void t1() {
+    public void plus_success() {
         int rs = Calc.run("3 + 5");
         assertThat(rs).isEqualTo(8);
     }
 
     @Test
     @DisplayName("10 / 5")
-    public void t2() {
+    public void divide_success() {
         int rs = Calc.run("10 / 5");
         assertThat(rs).isEqualTo(2);
     }
 
     @Test
+    @DisplayName("10 / 0")
+    public void divide_failure() {
+        assertThatThrownBy(() -> Calc.run("10 / 0"))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("0으로 나눌수 없음.");
+    }
+
+    @Test
     @DisplayName("20 * 5")
-    public void t3() {
+    public void multiplu_success() {
         int rs = Calc.run("20 * 5");
         assertThat(rs).isEqualTo(100);
     }
 
     @Test
     @DisplayName("20 - 5")
-    public void t4() {
+    public void minus_success() {
         int rs = Calc.run("20 - 5");
         assertThat(rs).isEqualTo(15);
     }
