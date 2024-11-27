17011db3e305577ad48ed572cf0674317b3c2ac8
ByeongGyu Jeon <cube9928@naver.com>
2024-11-27 09:33:56 +0900

    10/5 RED

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,11 +1,12 @@
 import com.ll.Calc;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTest {
+    // int rs = Calc.run("((3 + 5) * 5 + -10) * 10 / 5");
+
     @Test
     @DisplayName("3 + 5")
     public void t1() {
@@ -13,4 +14,10 @@ public class CalcTest {
         assertThat(rs).isEqualTo(8);
     }
 
+    @Test
+    @DisplayName("10 / 5")
+    public void t2() {
+        int rs = Calc.run("10 / 5");
+        assertThat(rs).isEqualTo(2);
+    }
 }
