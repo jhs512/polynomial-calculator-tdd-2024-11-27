052e0318a82075f53869afce11ec03b9ae66f39f
NRKim93 <ning9303@naver.com>
2024-11-27 22:37:21 +0900

    TDD : GREEN 단계 ( 기능구현은 아직 X, 단지 Calc.java에 int ans = 0선언 후 ans = 60으로 변경 후 retrun )

--- app/src/main/java/polynomialcalculator/Calc.java
+++ app/src/main/java/polynomialcalculator/Calc.java
@@ -6,27 +6,6 @@ import org.checkerframework.checker.units.qual.min;
 
 public class Calc {
 
-    public static int plus(int num1, int num2) {;
-
-        return num1 + num2;
-
-    } 
-
-    public static int mult(int num1, int num2) {
-
-        return num1 * num2; 
-    }
-
-    public static int min(int num1, int num2) {
-
-        return num1 - num2; 
-    }
-
-    public static int div(int num1, int num2) {
-
-        return num1 / num2; 
-    }
-    
     
     public static int run(String cal) {
         int ans = 0;
