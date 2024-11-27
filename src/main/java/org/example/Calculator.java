8744675387bebbd1ff16e7b7162da0fc0d7c391f
LeeKyoWon <kskw20@naver.com>
2024-11-27 11:17:50 +0900

    덧셈+곱셈 테스트

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Calculator {
 
     public static int run(String expr) {
@@ -14,13 +17,69 @@ public class Calculator {
         }
          */
 
-        // 곱셈 테스트
+        /* 곱셈 테스트
         int rs = 1;
         String[] numbers = expr.split("\\*");
         for(int i=0; i<numbers.length; i++){
             rs *= Integer.parseInt(numbers[i]);
         }
+         */
+
+        // 덧셈 + 곱셈 테스트
+        Deque<String> deque = new ArrayDeque<>();
+
+        for(int i=0; i<expr.length(); i++) {
+            char c = expr.charAt(i);
+            if(isNumber(c)) {
+                i = addNumberToDeque(deque, expr, i);
+            } else if (c == '*') {
+
+                int operand1 = Integer.parseInt(deque.remove());
+                i++;
+                i = addNumberToDeque(deque, expr, i);
+                int operand2 = Integer.parseInt(deque.remove());
+                deque.addFirst(String.valueOf(operand1*operand2));
+
+            } else if (c == '+') {
+                deque.addFirst(String.valueOf(c));
+            }
+        }
+
+        int rs = 0;
+        while(!deque.isEmpty()) {
+            int operand2 = Integer.parseInt(deque.remove());
+            String operator = deque.remove();
+            int operand1 = Integer.parseInt(deque.remove());
+            deque.addFirst(String.valueOf(operand1 + operand2));
+            if (deque.size() == 1) {
+                break;
+            }
+        }
+        rs = Integer.parseInt(deque.remove());
 
         return rs;
     }
+
+
+    public static boolean isNumber(char c){
+        return 48 <= c && c <= 57;
+    }
+
+    public static int addNumberToDeque(Deque<String> deque, String expr, int start) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = start;
+
+        while(i < expr.length() && isNumber(expr.charAt(i))) {
+            sb.append(expr.charAt(i));
+            i++;
+        }
+
+        i--;
+        deque.addFirst(sb.toString());
+        System.out.println(sb);
+        return i;
+    }
+
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,6 +29,24 @@ class CalculatorTest {
         int rs = Calculator.run("5*4*3");
 
         assertThat(rs).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("덧셈 + 곱셈 테스트")
+    public void t3() {
+        int rs = Calculator.run("53+3*4+3");
+
+        assertThat(rs).isEqualTo(68);
+    }
+
+    @Test
+    @DisplayName("Expr에서 숫자만 읽어서 Deque에 넣기")
+    public void t4() {
+        Deque<String> deque = new ArrayDeque<>();
+
+        Calculator.addNumberToDeque(deque, "1234abcd", 0);
+
+        assertThat(deque.remove()).isEqualTo("1234");
     }
 
 }
