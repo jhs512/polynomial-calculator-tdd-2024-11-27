efbdc3e990c2accb9aa46a3153865898ec13c9b9
vdvhk12 <vdvhk12@gmail.com>
2024-11-28 13:33:15 +0900

    CalculatorRecursion 테스트 케이스 8 실패

--- src/test/java/org/example/CalculatorRecursionTest.java
+++ src/test/java/org/example/CalculatorRecursionTest.java
@@ -22,4 +22,95 @@ class CalculatorRecursionTest {
         assertThat(result).isEqualTo(3);
     }
 
+    @Test
+    @DisplayName("2 + 1 == 3")
+    void t2() {
+        //given
+        String expression = "2 + 1";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("2 + 2 == 4")
+    void t3() {
+        //given
+        String expression = "2 + 2";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("1000 + 280 == 1280")
+    void t4() {
+        //given
+        String expression = "1000 + 280";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(1280);
+    }
+
+    @Test
+    @DisplayName("2 - 1 == 1")
+    void t5() {
+        //given
+        String expression = "2 - 1";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("3 - 1 == 2")
+    void t6() {
+        //given
+        String expression = "3 - 1";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("100 - 20 == 80")
+    void t7() {
+        //given
+        String expression = "100 - 20";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(80);
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    void t8() {
+        //given
+        String expression = "10 + 20 + 30";
+
+        //when
+        int result = calculatorRecursion.calculate(expression);
+
+        //then
+        assertThat(result).isEqualTo(60);
+    }
+
 }
