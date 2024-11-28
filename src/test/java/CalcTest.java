84eb90d0f061215a62340d322b315e998edba6a9
kdo_1999/mac <kdo_1999@naver.com>
2024-11-28 14:12:19 +0900

    test: CalcTest.t1() 수정

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -4,7 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.calc.Calc;
+
 public class CalcTest {
+	private Calc calc;
 	@Test
     @DisplayName("1 + 1 == 2")
     void t1() {
