51ad5c73974359d773b27abc04ea040c54a1adc0
kdo_1999/mac <kdo_1999@naver.com>
2024-11-28 14:48:14 +0900

    refactor: Calc.run 리팩토링

--- src/main/java/com/calc/Calc.java
+++ src/main/java/com/calc/Calc.java
@@ -1,8 +1,26 @@
 package com.calc;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class Calc {
 
 	public static int run(String exp) {
-		return 1 + 1;
+		int result = calc(exp);
+
+		return result;
+	}
+
+	private static int calc(String exp) {
+		boolean plusContains = exp.contains("+");
+
+		if (plusContains) {
+			String[] split = exp.split("\\+");
+
+			return Arrays.stream(split).mapToInt(s -> Integer.parseInt(s.trim())).sum();
+		}
+
+		return 0;
 	}
 }
