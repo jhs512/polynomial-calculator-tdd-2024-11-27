e55bd7f5cc7c122574dc3fb6bd561f02ce19e3bb
oungsi2000 <oungsi1000@gmail.com>
2024-11-28 09:12:35 +0900

    feat:토크나이저 완성

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,20 +1,25 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import javax.management.RuntimeOperationsException;
+import java.io.EOFException;
+import java.util.*;
+
 
 public class App {
 
-    private List<Integer> strToInts (String expression, String operator) {
+    private List<Integer> strToInts(String expression, String operator) {
         String[] items = expression.replaceAll("\\s+", "").split(operator);
         if (items.length <= 1) throw new IllegalArgumentException("");
 
         //배열의 각 문자열을 int형으로 변환
-        List<Integer> intItems = Arrays.stream(items).map(
-            Integer::parseInt
+        return Arrays.stream(items).map(
+            (item) -> {
+                if (item.contains("m")) {
+                    return Integer.parseInt(item.replace("m", "-"));
+                }
+                return Integer.parseInt(item);
+            }
         ).toList();
-        return intItems;
     }
 
     public int add(String expression) {
@@ -24,16 +29,161 @@ public class App {
 
     public int minus(String expression) {
         List<Integer> intItems = strToInts(expression, "-");
-        return intItems.subList(1,intItems.size()).stream().reduce(intItems.getFirst(), (result, a)->result-a);
+        return intItems.subList(1, intItems.size()).stream().reduce(intItems.getFirst(), (result, a) -> result - a);
     }
 
     public int multiply(String expression) {
         List<Integer> intItems = strToInts(expression, "\\*");
-        return intItems.subList(1,intItems.size()).stream().reduce(intItems.getFirst(), (result, a)->result*a);
+        return intItems.subList(1, intItems.size()).stream().reduce(intItems.getFirst(), (result, a) -> result * a);
     }
 
     public int divide(String expression) {
         List<Integer> intItems = strToInts(expression, "\\/");
-        return intItems.subList(1,intItems.size()).stream().reduce(intItems.getFirst(), (result, a)->result/a);
+        return intItems.subList(1, intItems.size()).stream().reduce(intItems.getFirst(), (result, a) -> result / a);
+    }
+
+}
+
+class Tokenizer {
+
+    private final String expression;
+    private final char[] parsed;
+    private List<Map<String, String>> tokens = new ArrayList<Map<String, String>>();
+    private int index = 0;
+    private int depth = 0;
+
+    Tokenizer(String expression) {
+        this.expression = expression;
+        this.parsed = expression.toCharArray();
+    }
+
+    private boolean advance() {
+        if (index == parsed.length-1) throw new RuntimeException("EOF");
+        index+=1;
+        return true;
+    }
+
+    private boolean hasNext() {
+        if (index == parsed.length-1) return false;
+        return true;
+    }
+
+    private void retreat() {
+        if (index == 0) return;
+        index-=1;
+    }
+
+    private String get() {
+        return String.valueOf(parsed[index]);
+    }
+
+    private String getNext() {
+        return String.valueOf(parsed[index+1]);
+    }
+
+
+    public List<Map<String, String>> tokenize() {
+
+        outerloop:
+        while (true) {
+            String arg = get();
+            Map<String, String> token = new HashMap<>();
+
+            //숫자 돌입
+            if ("0123456789".contains(arg)) {
+                StringBuilder valueBuilder = new StringBuilder();
+                while ("0123456789".contains(get())) {
+                    valueBuilder.append(get());
+                    if (!hasNext()) break;
+                    advance();
+                }
+                token.put("value", valueBuilder.toString());
+                token.put("attribute", "value");
+
+
+                if (!hasNext()) {
+                    tokens.add(token);
+                    if (")".contains(get())) {
+                        token.put("value", get());
+                        token.put("attribute", "close");
+                        token.put("depth", String.valueOf(depth));
+                        depth -= 1;
+                        tokens.add(token);
+                    }
+                    break;
+                }
+            }
+
+            //우선순위 높은 연산자 & 부호가 될 수 없는 연산자
+            if ("\\*\\/".contains(arg)) {
+                retreat();
+                if ("\\*\\/\\-\\+".contains(get())) throw new RuntimeException("잘못된 연산자 사용 index : " + index);
+                advance();
+
+                token.put("value", arg);
+                token.put("attribute", "operator");
+                token.put("priority", "0");
+                advance();
+            }
+
+            //우선순위 낮은 연산자 & 부호가 될 수 있는 연산자
+            if ("\\+\\-".contains(arg)) {
+                retreat();
+                if ("\\*\\/\\-\\+".contains(get())) {
+                    advance();
+
+                    StringBuilder valueBuilder = new StringBuilder();
+                    if (("-".contains(get()))) {
+                        valueBuilder.append("-");
+                        advance();
+                    } else advance();
+
+                    while ("0123456789".contains(get())) {
+                        valueBuilder.append(get());
+                        if (!hasNext()) break;
+                        advance();
+                    }
+                    token.put("value", valueBuilder.toString());
+                    token.put("attribute", "value");
+
+                    if (!hasNext()) {
+                        tokens.add(token);
+                        if (")".contains(get())) {
+                            token.put("value", get());
+                            token.put("attribute", "close");
+                            token.put("depth", String.valueOf(depth));
+                            depth -= 1;
+                            tokens.add(token);
+                        }
+                        break;
+                    }
+                }
+
+                advance();
+                token.put("value", arg);
+                token.put("attribute", "operator");
+                token.put("priority", "1");
+                advance();
+            }
+
+            if ("(".contains(arg)) {
+                depth += 1;
+                token.put("value", arg);
+                token.put("attribute", "open");
+                token.put("depth", String.valueOf(depth));
+                advance();
+            }
+
+            if (")".contains(arg)) {
+                token.put("value", arg);
+                token.put("attribute", "close");
+                token.put("depth", String.valueOf(depth));
+                depth -= 1;
+                advance();
+            }
+            tokens.add(token);
+        }
+        return tokens;
     }
 }
+
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.Map;
+
 public class AppTest {
     App app = new App();
 
@@ -17,6 +20,7 @@ public class AppTest {
         "5+3, 8",
         "5+4+7, 16",
         "5 + 3, 8",
+        "m5+3, -2"
     })
     public void addTest(String s, int i) {
        int result = app.add(s);
@@ -30,7 +34,8 @@ public class AppTest {
         "5-4-7, -6",
         "0-0, 0",
         "1-1-2, -2",
-        "5 - 3, 2"
+        "5 - 3, 2",
+        "m5-3, -8"
     })
     public void minusTest(String s, int i) {
         int result = app.minus(s);
@@ -43,7 +48,8 @@ public class AppTest {
         "5*3, 15",
         "2*2*3, 12",
         "4*4*4, 64",
-        "5 * 3, 15"
+        "5 * 3, 15",
+        "m3*5, -15"
     })
     public void multiplyTest(String s, int i) {
         int result = app.multiply(s);
@@ -60,6 +66,30 @@ public class AppTest {
     })
     public void divideTest(String s, int i) {
         int result = app.divide(s);
+        assertThat(result).isEqualTo(i);
+    }
+
+    @DisplayName("토큰화 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {
+//        "5+3: [{attribute=value, value=5}, {attribute=operator, priority=1, value=+}, {attribute=value, value=3}]",
+//        "5+3-2:[{attribute=value, value=5}, {attribute=operator, priority=1, value=+}, {attribute=value, value=3}, {attribute=operator, priority=1, value=-}, {attribute=value, value=2}]",
+//        "5+3+-2:[{attribute=value, value=5}, {attribute=operator, priority=1, value=+}, {attribute=value, value=3}, {attribute=operator, priority=1, value=+}, {attribute=value, value=-2}]",
+        "5*(3+1):dasd"
+    }, delimiter=':')
+    public void parseTest(String s, String i) {
+        Tokenizer tokenizer = new Tokenizer(s);
+        List<Map<String, String>> tokens = tokenizer.tokenize();
+        assertThat(tokens.toString()).isEqualTo(i);
+    }
+
+    @DisplayName("기능 테스트")
+    @ParameterizedTest
+    @CsvSource({
+        "((5+3)*-4), 4"
+    })
+    public void appTest(String s, int i) {
+        int result = ExpressionCalculator.calculate(s);
         assertThat(result).isEqualTo(i);
     }
 }
