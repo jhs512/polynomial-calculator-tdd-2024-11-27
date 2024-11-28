c9d421f8f2310f764f0a27e2a078da52f12a7438
tigerpoint123 <tigerrla@naver.com>
2024-11-28 17:42:35 +0900

    case 1 to 14

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -9,22 +9,70 @@ public class Calc {
         int[] num = new int[tokens.length];
         String[] operators = new String[tokens.length];
 
-        for(int i = 0; i < tokens.length; i++) {
-            if(i % 2 == 0) num[i] = Integer.parseInt(tokens[i]);
+        int numIndex = 2;
+        int operatorIndex = 1;
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (i % 2 == 0) num[i] = Integer.parseInt(tokens[i]);
             else operators[i] = tokens[i];
         }
 
-
-        switch (operators[1]) {
-            case "+":
-                return Optional.of(num[0] + num[2]).get();
-            case "-":
-                return Optional.of(num[0] - num[2]).get();
-            case "*":
-                return Optional.of(num[0] * num[2]).get();
-            case "/":
-                return Optional.of(num[0] / num[2]).get();
+        int result = num[0];
+        while (true) {
+            if (operatorIndex >= tokens.length || numIndex >= tokens.length) break;
+            switch (operators[operatorIndex]) {
+                case "+":
+                    result += num[numIndex];
+                    break;
+                case "-":
+                    result -= num[numIndex];
+                    break;
+                case "*":
+                    result *= num[numIndex];
+                    break;
+                case "/":
+                    result /= num[numIndex];
+                    break;
+            }
+            numIndex += 2;
+            operatorIndex += 2;
         }
-        return 0;
+        return Optional.of(result).get();
+    }
+
+    public static Integer run2(String str) {
+        String[] tokens = str.split(" ");
+        int[] num = new int[tokens.length];
+        String[] operators = new String[tokens.length];
+
+        int numIndex = 2;
+        int operatorIndex = 1;
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (i % 2 == 0) num[i] = Integer.parseInt(tokens[i]);
+            else operators[i] = tokens[i];
+        }
+
+        int result = num[0];
+        while (true) {
+            if (operatorIndex >= tokens.length || numIndex >= tokens.length) break;
+            switch (operators[operatorIndex]) {
+                case "+":
+                    result += num[numIndex];
+                    break;
+                case "-":
+                    result -= num[numIndex];
+                    break;
+                case "*":
+                    result *= num[numIndex];
+                    break;
+                case "/":
+                    result /= num[numIndex];
+                    break;
+            }
+            numIndex += 2;
+            operatorIndex += 2;
+        }
+        return Optional.of(result).get();
     }
 }
