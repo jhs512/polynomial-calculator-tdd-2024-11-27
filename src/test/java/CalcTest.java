6afe6388d73cc1b3f244da27c4bbaa7447360732
kdo_1999/mac <kdo_1999@naver.com>
2024-11-30 00:46:53 +0900

    test: CalcTest.t27 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -165,4 +165,9 @@ public class CalcTest {
         assertThat(Calc.run("-(8 + 2) * -(7 + 3) + 5")).isEqualTo(105);
     }
 
+	@Test
+    @DisplayName("5 - (1 + 5) == -1")
+    void t27() {
+        assertThat(Calc.run("5 - (1 + 5)")).isEqualTo(-1);
+    }
 }
