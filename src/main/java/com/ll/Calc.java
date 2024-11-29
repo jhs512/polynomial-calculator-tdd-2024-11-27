d1666162d2f6e5cb4790771515e37edea562c8f6
UCLABgown <lovecein4858@gmail.com>
2024-11-29 15:49:01 +0900

    주석

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,10 +1,7 @@
 package com.ll;
-
 import java.util.*;
 
-
 public class Calc {
-
 
     static double run(String str) {
         str = str.replaceAll("[(]", "( ");
@@ -14,6 +11,7 @@ public class Calc {
         result = Double.parseDouble(loop(arr).getFirst());
         return result;
     }
+
     //첫"("을 찾는데 쓰는함수 재활용 딱히 안하는데 그냥 나눔
     static public int containsIndex(List<String> arr, String ward, int startIndex) {
         int start = startIndex;
@@ -23,9 +21,9 @@ public class Calc {
         }
         return -1;
     }
+
     //((1+1)) + (1+1)이렇게 있으면 "("(1+1)")" + (1+1) 저거 두개 인덱스 가져오는 함수
     static public HashMap<String, Integer> ParenIndex(List<String> arr) {
-
         HashMap<String, Integer> parenIndex = new HashMap<>();
         int start = containsIndex(arr, "(", 0);
         if (start == -1) return new HashMap<>();
@@ -46,10 +44,8 @@ public class Calc {
 
     // 재귀함수 "(" ")" 있으면 그부분만 잘라서 다시 for문 넣은건 (())는 괜찮은데 () + () 있을때 앞부분만 계산하고 끝나서
     static List<String> loop(List<String> arr) {
-
         HashMap<String, Integer> parenIndex = ParenIndex(arr);
         for (; parenIndex.containsKey("start"); parenIndex = ParenIndex(arr)) {
-
             int start = parenIndex.get("start");
             int end = parenIndex.get("end");
             List<String> newArr = loop(new ArrayList<>(arr.subList(start + 1, end)));
@@ -58,12 +54,11 @@ public class Calc {
             if (end >= start) arr.subList(start, end + 1).clear();
             arr.addAll(start, newArr);
         }
-        if(calculate(arr))
+        if (calculate(arr))
             loop(arr);
-
-
         return arr;
     }
+
     //계산 외부에서 계산하려고했는데 함수 나눠야해서 그냥 여기서 처리
     public static boolean calculate(List<String> arr) {
         int index = -1;
@@ -86,6 +81,5 @@ public class Calc {
         arr.remove(index - 1);
         return true;
     }
-
 
 }
