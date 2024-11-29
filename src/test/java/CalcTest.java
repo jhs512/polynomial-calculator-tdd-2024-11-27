8596159c13d8fa6150aede9e1830351c93103ceb
kdo_1999/mac <kdo_1999@naver.com>
2024-11-29 23:55:16 +0900

    test: CalcTest.t20 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -123,5 +123,11 @@ public class CalcTest {
         assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
     }
 
+	@Test
+    @DisplayName("(((10 + 20))) == 30")
+    void t20() {
+        assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
+    }
+
 
 }
