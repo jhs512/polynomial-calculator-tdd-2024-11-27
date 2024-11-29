8dedb0b371d73e8339e34f37885808a1ca1b38e0
kdo_1999/mac <kdo_1999@naver.com>
2024-11-30 00:42:24 +0900

    test: CalcTest.t25 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -153,4 +153,11 @@ public class CalcTest {
         assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
     }
 
+	@Test
+    @DisplayName("-(10 + 5) == -15")
+    void t25() {
+        assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
+    }
+
+
 }
