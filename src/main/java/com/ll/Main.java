a774d84d73187737c9e7faf0d1a5f9113e1181d4
Daewook123 <cdo5080@gmail.com>
2024-11-29 11:10:31 +0900

    Green

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,6 +4,7 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Calc calc = new Calc();
-        calc.run("");
+        int run = calc.run("3 * 1 + -3");
+        System.out.println(run);
     }
 }
