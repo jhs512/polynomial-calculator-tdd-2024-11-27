5cb25c863fe3423fd77aafe0e83f7559b6f68031
kdo_1999/mac <kdo_1999@naver.com>
2024-11-29 23:11:35 +0900

    feat: Calc.calc minus 추가

--- src/main/java/com/calc/Calc.java
+++ src/main/java/com/calc/Calc.java
@@ -25,7 +25,7 @@ public class Calc {
 
 		for (int i = 0; i < exp.length; i++) {
 			exp[i] = exp[i].trim();
-			if (exp[i].equals("+")) {
+			if (exp[i].equals("+") || exp[i].equals("-")) {
 				operatorIndex = i;
 			}
 		}
@@ -34,10 +34,17 @@ public class Calc {
 			case "+":
 				int sum = sum(Integer.parseInt(exp[operatorIndex - 1]), Integer.parseInt(exp[operatorIndex + 1]));
 
-				exp[operatorIndex - 1] = String.valueOf(sum);
-				exp[operatorIndex] = "";
-				exp[operatorIndex + 1] = "";
+				resultInsert(exp, operatorIndex, sum);
 				shiftArray(exp, operatorIndex);
+
+				break;
+			case "-":
+				int minus = minus(Integer.parseInt(exp[operatorIndex - 1]), Integer.parseInt(exp[operatorIndex + 1]));
+
+				resultInsert(exp, operatorIndex, minus);
+
+				break;
+
 		}
 
 		System.out.print("Calc.calc= ");
@@ -48,8 +55,18 @@ public class Calc {
 		}
 	}
 
+	private static void resultInsert(String[] exp, int operatorIndex, int sum) {
+		exp[operatorIndex - 1] = String.valueOf(sum);
+		exp[operatorIndex] = "";
+		exp[operatorIndex + 1] = "";
+	}
+
 	private static int sum(int a, int b) {
 		return a + b;
+	}
+
+	private static int minus(int a, int b) {
+		return a - b;
 	}
 
 	// 배열 축소: 처리된 값을 제거하고 나머지 값을 앞으로 이동
