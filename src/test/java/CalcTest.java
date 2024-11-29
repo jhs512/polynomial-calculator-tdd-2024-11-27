8fc1e8afbf43d4bef520ba213eff0fa04fe9a398
kdo_1999/mac <kdo_1999@naver.com>
2024-11-29 11:18:25 +0900

    test: CalcTest.t12 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -8,69 +8,77 @@ import com.calc.Calc;
 
 public class CalcTest {
 	private Calc calc;
-	@Test
-    @DisplayName("1 + 1 == 2")
-    void t1() {
-        assertThat(Calc.run("1 + 1")).isEqualTo(2);
-    }
 
 	@Test
-    @DisplayName("2 + 1 == 3")
-    void t2() {
-        assertThat(Calc.run("2 + 1")).isEqualTo(3);
-    }
+	@DisplayName("1 + 1 == 2")
+	void t1() {
+		assertThat(Calc.run("1 + 1")).isEqualTo(2);
+	}
 
 	@Test
-    @DisplayName("2 + 2 == 4")
-    void t3() {
-        assertThat(Calc.run("2 + 2")).isEqualTo(4);
-    }
+	@DisplayName("2 + 1 == 3")
+	void t2() {
+		assertThat(Calc.run("2 + 1")).isEqualTo(3);
+	}
 
 	@Test
-    @DisplayName("1000 + 280 == 1280")
-    void t4() {
-        assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
-    }
+	@DisplayName("2 + 2 == 4")
+	void t3() {
+		assertThat(Calc.run("2 + 2")).isEqualTo(4);
+	}
 
 	@Test
-    @DisplayName("2 - 1 == 1")
-    void t5() {
-        assertThat(Calc.run("2 - 1")).isEqualTo(1);
-    }
+	@DisplayName("1000 + 280 == 1280")
+	void t4() {
+		assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
+	}
 
 	@Test
-    @DisplayName("3 - 1 == 2")
-    void t6() {
-        assertThat(Calc.run("3 - 1")).isEqualTo(2);
-    }
+	@DisplayName("2 - 1 == 1")
+	void t5() {
+		assertThat(Calc.run("2 - 1")).isEqualTo(1);
+	}
 
 	@Test
-    @DisplayName("100 - 20 == 80")
-    void t7() {
-        assertThat(Calc.run("100 - 20")).isEqualTo(80);
-    }
+	@DisplayName("3 - 1 == 2")
+	void t6() {
+		assertThat(Calc.run("3 - 1")).isEqualTo(2);
+	}
 
 	@Test
-    @DisplayName("10 + 20 + 30 == 60")
-    void t8() {
-        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
-    }
+	@DisplayName("100 - 20 == 80")
+	void t7() {
+		assertThat(Calc.run("100 - 20")).isEqualTo(80);
+	}
 
 	@Test
-    @DisplayName("10 - 20 + 30 == 20")
-    void t9() {
-        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
-    }
+	@DisplayName("10 + 20 + 30 == 60")
+	void t8() {
+		assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+	}
 
 	@Test
-    @DisplayName("10 - 10 - 10 - 10 == -20")
-    void t10() {
-        assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
-    }
+	@DisplayName("10 - 20 + 30 == 20")
+	void t9() {
+		assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+	}
 
 	@Test
-    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
-    void t11() {
-        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
-    }
+	@DisplayName("10 - 10 - 10 - 10 == -20")
+	void t10() {
+		assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+	}
+
+	@Test
+	@DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+	void t11() {
+		assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+	}
+
+	@Test
+	@DisplayName("10 * 10 == 100")
+	void t12() {
+		assertThat(Calc.run("10 * 10")).isEqualTo(100);
+	}
+
 }
