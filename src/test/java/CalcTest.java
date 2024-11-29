30c1bbde56640cb67ec1d5d0ced332c5eaf79b77
kdo_1999/mac <kdo_1999@naver.com>
2024-11-29 23:56:24 +0900

    test: CalcTest.t21 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -112,22 +112,27 @@ public class CalcTest {
 	}
 
 	@Test
-    @DisplayName("(10 + 20) == 30")
-    void t18() {
-        assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
-    }
+	@DisplayName("(10 + 20) == 30")
+	void t18() {
+		assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
+	}
 
 	@Test
-    @DisplayName("((10 + 20)) == 30")
-    void t19() {
-        assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
-    }
+	@DisplayName("((10 + 20)) == 30")
+	void t19() {
+		assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
+	}
 
 	@Test
-    @DisplayName("(((10 + 20))) == 30")
-    void t20() {
-        assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
-    }
+	@DisplayName("(((10 + 20))) == 30")
+	void t20() {
+		assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
+	}
 
+	@Test
+	@DisplayName("(20 + 20) + 20 == 60")
+	void t21() {
+		assertThat(Calc.run("(20 + 20) + 20")).isEqualTo(60);
+	}
 
 }
