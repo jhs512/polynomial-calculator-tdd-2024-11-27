0b6e7ed258eda6465fc14db6133ade7f8cccd867
kdo_1999/mac <kdo_1999@naver.com>
2024-11-29 23:03:02 +0900

    feat: Calc.calc sum 추가

--- src/main/java/com/calc/Calc.java
+++ src/main/java/com/calc/Calc.java
@@ -6,10 +6,58 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class Calc {
+	private static String[] expArray;
 
 	public static int run(String exp) {
-		int result = calc(exp);
+		expArray = exp.split(" ");
 
-		return result;
+		System.out.print("Calc.run= ");
+		Arrays.stream(expArray).forEach((s) -> System.out.print(s + " "));
+		System.out.print("\n");
+
+		calc(expArray);
+
+		return Integer.parseInt(expArray[0]);
+	}
+
+	private static void calc(String[] exp) {
+		int operatorIndex = 0;
+
+		for (int i = 0; i < exp.length; i++) {
+			exp[i] = exp[i].trim();
+			if (exp[i].equals("+")) {
+				operatorIndex = i;
+			}
+		}
+
+		switch (exp[operatorIndex]) {
+			case "+":
+				int sum = sum(Integer.parseInt(exp[operatorIndex - 1]), Integer.parseInt(exp[operatorIndex + 1]));
+
+				exp[operatorIndex - 1] = String.valueOf(sum);
+				exp[operatorIndex] = "";
+				exp[operatorIndex + 1] = "";
+				shiftArray(exp, operatorIndex);
+		}
+
+		System.out.print("Calc.calc= ");
+		Arrays.stream(expArray).forEach((s) -> System.out.print(s + " "));
+
+		if (!exp[1].isBlank()) {
+			calc(exp);
+		}
+	}
+
+	private static int sum(int a, int b) {
+		return a + b;
+	}
+
+	// 배열 축소: 처리된 값을 제거하고 나머지 값을 앞으로 이동
+	private static void shiftArray(String[] stringArray, int index) {
+		for (int i = index + 2; i < stringArray.length; i++) {
+			stringArray[i - 2] = stringArray[i];
+		}
+		stringArray[index] = "";
+		stringArray[index + 1] = "";
 	}
 }
